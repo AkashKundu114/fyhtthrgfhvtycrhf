5e19914bf6c6786582246e15080159672c99c3c6
Glitch (rhinestone-brass-triangle) <none>
2020-12-15 08:29:42 +0000

    ⚓️🏁 Checkpoint ./package.json:22669217/32 ./config.json:22669217/104 ./stock/nordvpn.txt:22669217/104 ./stock/minecraft.txt:22669217/104 ./commands/help.js:22669217/8

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
 .setColor("#0x9700f5")
 .setDescription("HELP Command") 
-.addField('__Help__','__**Bermuda Generator Commands**__\n\n`$spotify` To Get A Spotify Account. \n`$hulu` To Get A Hulu Account. \n`$minecraft` To Get A Minecraft Account. \n`$nordvpn` To Get A NordVPN Account. \n`$origin` To Get A Origin Account.')
+.addField('__Help__','__**Generator Commands**__\n\n`$spotify` To Get A Spotify Account. \n`$hulu` To Get A Hulu Account. \n`$minecraft` To Get A Minecraft Account. \n`$nordvpn` To Get A NordVPN Account. \n`$origin` To Get A Origin Account.')
 .setThumbnail('https://i.imgur.com/AR9iKTR.png')
 .setFooter('By Bermuda™️')
 message.channel.send(embed)
