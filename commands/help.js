96c35e5f10fdb070448e6e563e04e2803f812e42
Glitch (rhinestone-brass-triangle) <none>
2020-12-16 05:47:24 +0000

    💵📰 Checkpoint ./commands/help.js:22669217/4251

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,28 @@
-const Discord = require ("discord.js")
+const Discord = require("discord.js");
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-.setColor("#0x9700f5")
-.setDescription("HELP Command") 
-.addField('__Help__','__**Generator Commands**__\n\n`$spotify` To Get A Spotify Account. \n`$hulu` To Get A Hulu Account. \n`$minecraft` To Get A Minecraft Account. \n`$nordvpn` To Get A NordVPN Account. \n`$origin` To Get A Origin Account.')
-.setThumbnail('https://i.imgur.com/AR9iKTR.png')
-.setFooter('By Bermuda™️')
-message.channel.send(embed)
-}
+    .setColor("#0x9700f5")
+    .setTitle("Accounts Stock")
+    .addField("Amazon", "`5000`", true)
+    .addField("Disney", "`5000`", true)
+    .addField("Fortnite", "`5000`", true)
+    .addField("Itunes", "`5000`", true)
+    .addField("Hulu", "`5000`", true)
+    .addField("Minecraft", "`5000`", true)
+    .addField("Netflix", "`5000true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .addField("Amazon", "`5000`", true)
+    .setThumbnail("")
+    .setFooter("Accounts Generator")
+    .setTimestamp();
+  message.channel.send(embed);
+};
 module.exports.help = {
-  name: 'help'
-}
+  name: "help"
+};
