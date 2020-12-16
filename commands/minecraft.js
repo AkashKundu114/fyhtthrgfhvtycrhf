6427bd305b94218b6a85f85c7f77dce3b746156d
Glitch (rhinestone-brass-triangle) <none>
2020-12-16 06:32:28 +0000

    🍧🏁 Checkpoint ./commands/help.js:22669217/1974 ./commands/hulu.js:22669217/11574 ./commands/minecraft.js:22669217/3487

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -1,54 +1,67 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
 const cooldown = new Set();
 
 module.exports.run = async (client, msg, args, config) => {
-    if(cooldown.has(msg.author.id)) {
-        msg.reply(`You need to wait ${config.COOLDOWN} minutes to use this command again!`)
-            .then((m) => {
-                msg.delete();
+  if (cooldown.has(msg.author.id)) {
+    msg
+      .reply(
+        `You need to wait ${config.COOLDOWN} minutes to use this command again!`
+      )
+      .then(m => {
+        msg.delete();
 
-                setTimeout(() => {
-                    m.delete();
-                }, 5000);
-            });
-    } else {
-        fs.readFile('./stock/minecraft.txt', 'utf8', function(err, data) {
-            if (err) throw err;
+        setTimeout(() => {
+          m.delete();
+        }, 5000);
+      });
+  } else {
+    fs.readFile("./stock/minecraft.txt", "utf8", function(err, data) {
+      if (err) throw err;
 
-            data = data + '';
-            var lines = data.split('\n');
-            let account = lines[Math.floor(Math.random() * 1)];
+      data = data + "";
+      var lines = data.split("\n");
+      let account = lines[Math.floor(Math.random() * 1)];
 
-            fs.writeFile('./stock/minecraft.txt', lines.slice(1).join('\n'), function(err) {
-                if(err) throw err;
-            });
+      fs.writeFile("./stock/minecraft.txt", lines.slice(1).join("\n"), function(
+        err
+      ) {
+        if (err) throw err;
+      });
 
-            let embed = new Discord.RichEmbed()
-            .addField('Minecraft account',`\n**${account}**`)
-            .setThumbnail('https://i.imgur.com/VGr3nXi.png')
-            .setColor("#0x9700f5")
-            .setFooter('By Bermuda')
-            .setTimestamp();
+      let embed = new Discord.RichEmbed()
+        .addField("🏓 Account", `\n\`${account}\``)
+        .addField("🔗 Links", "[Support Server](https://discord.gg/)")
+        .setThumbnail("https://i.imgur.com/GwgkSC4.png")
+        .setColor("RANDOM")
+        .setFooter("Helix Network")
+        .setTimestamp();
 
-            msg.author.send(embed);
+      msg.author.send(embed);
 
-            msg.reply('Check your DMS!')
-                .then(m => {
-                    setTimeout(() => {
-                        m.delete();
-                    }, 900000);
-                });
+      let reply = new Discord.RichEmbed()
+        .addField("✅ Generate", "**Check Your DMs!**")
+        .addField("🔗 Links", "[Support Server](https://discord.gg/)")
+        .setThumbnail("https://i.imgur.com/GwgkSC4.png")
+        .setColor("RANDOM")
+        .setFooter("Helix Network")
+        .setTimestamp();
 
-            cooldown.add(msg.author.id);
-            setTimeout(() => {
-                cooldown.delete(msg.author.id);
-            }, config.COOLDOWN * 60 * 1000);
-        });
-    }
+      msg.channel.send(reply).then(m => {
+        setTimeout(() => {
+          m.delete();
+        }, 900000);
+      });
+
+      cooldown.add(msg.author.id);
+      setTimeout(() => {
+        cooldown.delete(msg.author.id);
+      }, config.COOLDOWN * 60 * 1000);
+    });
+  }
 };
 
 module.exports.help = {
-    name: `minecraft`,
-    description: `Sends you a Minecraft Premium account!`
+  name: `minecraft`,
+  description: `description`
 };
